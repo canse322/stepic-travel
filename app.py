6aadd9ece53799dee6d1657bf97a1aed0d19376c
canse <canse.322@gmail.com>
2021-08-29 16:49:12 +0600

    added routes and base.html

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def render_main():
+    return render_template('index.html')
+
+
+@app.route('/departures/<departure>/')
+def render_departure(departure):
+    return render_template('departure.html')
+
+
+@app.route('/tours/<int:tour_id>/')
+def render_tour(tour_id):
+    return render_template('tour.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
